0496f070749fba64962816d78325f86ba8f4d082
wonjun9090 <wonjun9090@naver.com>
2021-03-08 17:58:56 +0900

    2021_03_08 | projects layout

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 23%;
+  margin: 2%;
+  height: 400px;
+  display: inline-block;
+  background-color: red;
+`
+
+export default Card
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import ImageBox from '../ImageBox';
+import TextBox from '../TextBox';
+import Card from '../Card';
+
+
+const ProjectsWrqpper = styled.div`
+  background-color: rgb(28, 191, 123);
+`
 
 
 function Projects(): React.ReactElement {
   return (
-    <div>
-      <span>인사인사인사</span>
-      <ul>
-        <li>프로젝트</li>
-        <li>프로젝트</li>
-        <li>프로젝트</li>
-        <li>프로젝트</li>
-        <li>프로젝트</li>
-        <li>프로젝트</li>
-      </ul>
-    </div>
+    <ProjectsWrqpper>
+      <TextBox>인사인사인사</TextBox>
+      <div>
+        <Card>
+          <ImageBox width="100%" height="30%" ImgUrl="GitHub.png"/>
+          <TextBox>간단소개</TextBox>
+        </Card>
+        <Card>
+          <ImageBox width="100%" height="30%" ImgUrl="GitHub.png"/>
+          <TextBox>간단소개</TextBox>
+        </Card>
+        <Card>
+          <ImageBox width="100%" height="30%" ImgUrl="GitHub.png"/>
+          <TextBox>간단소개</TextBox>
+        </Card>
+        <Card>
+          <ImageBox width="100%" height="30%" ImgUrl="GitHub.png"/>
+          <TextBox>간단소개</TextBox>
+        </Card>
+        <Card>
+          <ImageBox width="100%" height="30%" ImgUrl="GitHub.png"/>
+          <TextBox>간단소개</TextBox>
+        </Card>
+        <Card>
+          <ImageBox width="100%" height="30%" ImgUrl="GitHub.png"/>
+          <TextBox>간단소개</TextBox>
+        </Card>
+      </div>
+    </ProjectsWrqpper>
   )
 }
 
