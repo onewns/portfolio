9f654b8b9f937ac93e886ccfb26a1622beab72c8
wonjun9090 <wonjun9090@naver.com>
2021-03-15 13:37:12 +0900

    2921_03_15 | deploy gh-page

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
     <App />
     </BrowserRouter>
   </React.StrictMode>,
